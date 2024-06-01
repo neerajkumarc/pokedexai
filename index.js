fd1e49d39c4e262c64f68c0e341e60c5037ee012
neerajkumarc <chaudharyneeraj91@gmail.com>
2024-06-01 22:12:00 +0530

    Ready, Set, Go.

---
+++ index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require('express');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const path = require('path');
+const fs = require("fs")
+const app = express();
+const port = process.env.PORT || 3000;
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+app.use(express.json({ limit: '100mb' }))
+app.use(express.static(path.join(__dirname, 'public')));
+app.post('/pokedexit', async (req, res) => {
+    let { imageData } = req.body
+    try {
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+        const prompt = [`You are a Pokedex designed to output text. 
+        Given an image you should output the follwoing text:
+        {"description","object", "species", "approximateWeight", "approximateHeight", "hp", "attack", "defense", "speed", "type"}. 
+          
+        For example, if the object is a Golden Retriever, you should output the following text:
+        {"description":"Golden Retriever, a loyal and friendly canine species, known for its intelligence and adaptability. Weighing between 10-20 kg and standing 50-60 cm tall, this dog is a perfect companion for families. With a balanced attack and defense, and a moderate speed, Golden Retrievers excel in tasks that require endurance and strength. Their golden coat and gentle demeanor make them easily recognizable and beloved by many.","object": "Golden Retriever", "species": "Dog", "approximateWeight": "10-20 kg", "approximateHeight": "50-60 cm", "hp": 50, "attack": 40, "defense": 40, "speed": 19, "type": "normal"}.
+
+        Another example for a  {"description":"The Magpie, a sleek and intelligent avian species, renowned for its striking black and white plumage and its melodious calls. Weighing between 130 - 270 grams and standing 37-43 cm tall, this bird possesses remarkable agility and speed, making it a master of the skies. With moderate HP and attack stats, coupled with a slightly lower defense, Magpies rely on their swift movements and sharp beaks to outmaneuver and peck at their prey. Often found in urban and rural environments alike, these birds are both admired for their beauty and feared for their mischievous behavior","object": "Magpie", "species": "Bird", "approximateWeight": "130 - 270 g", "approximateHeight": "37-43 cm", "hp": 25, "attack": 20, "defense": 10, "speed": 32, "type": "Flying"}
+         
+        If you are given an object that is not a living creature, plant or lifeform, such as a coffee cup, output the same fields but with type: "Inanimate". 
+
+        If you are given a description of a person or human, output "species": "Human" and "name": "Person' and "type": "Normal". 
+        
+        If you are not sure what the attributes are for things like height or speed, it is okay to guess.
+       
+        `, {"inlineData":{"data":imageData, "mimeType":"image/*"}}];
+
+        // const prompt = ["what is shown in the image", ]
+
+        const result = await model.generateContent(prompt);
+        const text = result.response.text();
+        const jsonString = text.replace(/^```json\s*|```$/g, '')
+        res.json(JSON.parse(jsonString));
+        // res.send(text)
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: "Sorry, request can't be processed at the moment." });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ public/script.js
@@ -0,0 +1,79 @@
+const video = document.getElementById('video');
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
+const captureBtn = document.getElementById("captureBtn")
+const backBtn = document.getElementById("backBtn")
+const captureScreen = document.getElementById("captureScreen")
+const infoScreen = document.getElementById("infoScreen")
+
+navigator.mediaDevices.getUserMedia({
+    video: {
+        facingMode: { exact: "environment" },
+    },
+})
+    .then(stream => {
+        video.srcObject = stream;
+    })
+    .catch(error => {
+        console.error('Error accessing the camera:', error);
+    });
+
+function capturePhoto() {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    document.getElementById("image").setAttribute("src", canvas.toDataURL('image/png'))
+    const base64String = canvas.toDataURL('image/png').split(',')[1];
+    video.pause()
+    video.style.animation = "blinkani 1s ease-in-out infinite alternate"
+    captureBtn.setAttribute("disabled", "true")
+    sendToEndpoint(base64String);
+}
+
+function sendToEndpoint(base64String) {
+    const endpointUrl = '/pokedexit';
+    const requestData = { imageData: base64String };
+
+    fetch(endpointUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            // document.getElementById('response').textContent = JSON.stringify(data, null, 2);
+            renderInfo(data)
+        })
+        .catch(error => {
+            console.error('Error sending data to endpoint:', error);
+        });
+}
+
+function renderInfo(data, base64String) {
+    captureScreen.style.display = "none"
+    infoScreen.style.display = "block"
+    backBtn.style.display = "block"
+    document.getElementById("description").textContent = data.description;
+    document.getElementById("object").textContent = data.object;
+    document.getElementById("species").textContent = data.species;
+    document.getElementById("approximateWeight").textContent = data.approximateWeight;
+    document.getElementById("approximateHeight").textContent = data.approximateHeight;
+    document.getElementById("hp").textContent = data.hp;
+    document.getElementById("attack").textContent = data.attack;
+    document.getElementById("defense").textContent = data.defense;
+    document.getElementById("speed").textContent = data.speed;
+    document.getElementById("type").textContent = data.type;
+}
+
+backBtn.addEventListener("click", () => {
+    infoScreen.style.display = "none"
+    captureScreen.style.display = "block"
+    backBtn.style.display = "none"
+    video.play()
+    video.style.animation = ""
+    captureBtn.removeAttribute("disabled")
+})
